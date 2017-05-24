39381416722b10805194608952c72cdd4bdf964b
Tal <talrab@gmail.com>
2017-05-24 15:08:31 +0300

    adding a unit test which uses appium

---
+++ Appium1.py
@@ -0,0 +1,47 @@
+###############################################################
+#
+#
+###############################################################
+
+#from selenium import webdriver
+from appium import webdriver
+import unittest
+
+
+class CalcAndroidTests(unittest.TestCase):
+    "Class to run tests against the calculator Free app"
+
+    def setUp(self):
+        "Setup for the test"
+        desired_caps = {}
+        desired_caps['platformName'] = 'Android'
+        desired_caps['platformVersion'] = '6.0'
+        desired_caps['deviceName'] = 'Android Emulator'
+        desired_caps['appPackage'] = 'com.android.calculator2'
+        desired_caps['appActivity'] = '.Calculator'
+        self.driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
+
+    def tearDown(self):
+        "Tear down the test"
+        self.driver.quit()
+
+    def test_addition(self):
+        "Test a simple add calculation"
+        self.driver.find_element_by_name("7").click()
+        self.driver.find_element_by_name("+").click()
+        self.driver.find_element_by_id("digit_8").click()
+        result_element = self.driver.find_element_by_id("result")
+        self.assertEqual('15',result_element.text)
+
+    def test_subtraction(self):
+        "Test a simple subtraction calculation"
+        self.driver.find_element_by_name("9").click()
+        self.driver.find_element_by_id("op_sub").click()
+        self.driver.find_element_by_id("digit_6").click()
+        result_element = self.driver.find_element_by_id("result")
+        self.assertEqual('3',result_element.text)
+
+# ---START OF SCRIPT
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(CalcAndroidTests)
+    unittest.TextTestRunner(verbosity=2).run(suite)
